18273f8939acc47a52c1909a2766a2993a4b6628
Roman <roman.strus@gmail.com>
2018-03-14 23:44:57 +0200

    deleted home page

--- src/components/Orlyk/Orlyk.vue
+++ src/components/Orlyk/Orlyk.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-var RegLogHeader = require("../globals/header.vue");
+var Header = require("../globals/header.vue");
 
 export default {
   name: "Orlyk",
   components: {
-    "section-header": RegLogHeader
+    "section-header": Header
   }
 };
 </script>
